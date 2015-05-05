df559245e3addd9496b9606d195c0060585b1fff
baizhima <oh_baizhima@163.com>
2015-05-05 12:25:02 +0800

    final Kaggle code

--- runkaggle.R
+++ runkaggle.R
@@ -13,20 +13,28 @@ Sys.setenv(LANG="en")
 library(tm)
 library(SnowballC)
 library(caTools)
+library(ROCR)
 library(rpart)
+library(caret)
 library(randomForest)
+library(e1071) # svm
 
 # 1.2 Load Training/Test datasets
 # UniqueID: Train 1-6532, Test 6533-8402
 setwd("~/Desktop/coursera/analyticsEdge/kaggle/")
 Train = read.csv("NYTimesBlogTrain.csv",stringsAsFactors=FALSE)
 Test = read.csv("NYTimesBlogTest.csv",stringsAsFactors=FALSE)
-trainID = c(min(Train$UniqueID),max(Train$UniqueID))
-testID = c(min(Test$UniqueID),max(Test$UniqueID))
+testID = Test$UniqueID
 
 # 1.3 Merge Train and Test set into Data
 Test$Popular = 2
 Data = rbind(Train, Test)
+
+# 1.4 Find patterns on Headline(research purposes)
+popularData = subset(Data, Popular == 1)
+unpopularData = subset(Data, Popular == 0)
+
+ 
 
 
 ### 2. Build Abstract Corpus
@@ -51,7 +59,7 @@ corpusA = tm_map(corpusA, removePunctuation)
 corpusA = tm_map(corpusA, removeWords, stopwords("english"))
 corpusA = tm_map(corpusA, stemDocument)
 frequenciesA = DocumentTermMatrix(corpusA)
-sparseA = removeSparseTerms(frequenciesA, 0.99) # 223 words remained
+sparseA = removeSparseTerms(frequenciesA, 0.985) # 223 words remained 0.99 
 AbstractSparse = as.data.frame(as.matrix(sparseA))
 colnames(AbstractSparse) = paste0("a_",colnames(AbstractSparse))
 AbstractSparse$UniqueID = Data$UniqueID
@@ -66,6 +74,7 @@ allData = merge(Data, wordSparse, by="UniqueID")
 allData$Abstract = NULL
 allData$Snippet = NULL
 allData$Headline = NULL 
+allData$UniqueID = NULL
 
 # 3.3 Factorize section/subsetction name strings
 allData$NewsDesk = as.factor(allData$NewsDesk)
@@ -81,7 +90,7 @@ allData$PubDate = NULL
 
 # 3.5 split word counts into different levels by quartiles
 quartiles = summary(allData$WordCount)
-wordsLevel = c(quartiles["1st Qu."],quartiles["Median"],quartiles["3rd Qu."]) 
+wordsLevel = c(quartiles["1st Qu."], quartiles["Median"],quartiles["3rd Qu."])
 allData$LenType = as.integer(allData$WordCount > wordsLevel[1])
 allData$LenType = allData$LenType + as.integer(allData$WordCount > wordsLevel[2])
 allData$LenType = allData$LenType + as.integer(allData$WordCount > wordsLevel[3])
@@ -93,37 +102,100 @@ allData = allData[, order(names(allData))]
 
 # 3.7 Split Train/Test data by Popular
 blogTrain = subset(allData, Popular <= 1)
+set.seed(20)
+spl = sample.split(blogTrain$Popular, SplitRatio = 0.9)
+pureTrain = subset(blogTrain, spl==TRUE)
+pureVal = subset(blogTrain, spl==FALSE)
 blogTest = subset(allData, Popular == 2)
 blogTest$Popular = NULL # to be predicted
 
+### 4. Model 4: Logistic Regression
+# 4.1 Build LR model
+modelLR = glm(Popular ~ ., data=pureTrain, family="binomial")
 
-### 4. Model 1: Classification Tree(CART)
-# 4.1 Build CART model
-modelCART = rpart(Popular ~ . - UniqueID, data=blogTrain,method='class')
+# 4.2 Make predictions on validation data
+predictLR = predict(modelLR, newdata=pureVal, type='response')
+predLR = prediction(predictLR, pureVal$Popular)
+aucLR = as.numeric(performance(predLR, "auc")@y.values)
+aucLR # 0.9249
 
-# 4.2 Make predictions on training data
-predictCART = predict(modelCART, data=blogTrain, type='class')
-confusionCART = table(blogTrain$Popular, predictCART) # training set accuracy 0.906
-trainAccuracyCART = (confusionCART[1,1]+confusionCART[2,2])/sum(confusionCART)
+# 4.3 Build LR model using (pureTrain + pureVal) data
+modelLR = glm(Popular ~ ., data=blogTrain, family="binomial")
 
-# 4.3 Write predictions into CSV file "predictCART.csv"
-predictCART2 = predict(modelCART, newdata=blogTrain, type='class')
-submissionCART = data.frame(UniqueID=blogTest$UniqueID, Probability1=predictCART2)
-write.csv(submissionCART, "submissionCART.csv", row.names=FALSE)
-
-
+# 4.4 Write predictions into CSV file "submissionRF.csv"
+predictLR2 = predict(modelLR, newdata=blogTest, type='response')
+predictLR2[predictLR2 > 1] = 1.00 # fix floating-point arithmetic errors
+predictLR2[predictLR2 < 0] = 0.00 # fix floating-point arithmetic errors
+submissionLR = data.frame(UniqueID=testID, Probability1= predictLR2)
+write.csv(submissionLR, "submissionLR.csv", row.names=FALSE)
 
 ### 5. Model 2: Random Forest
-# 5.1 Build RF model
-modelRF = randomForest(Popular ~ . - UniqueID, data=blogTrain, nodesize=25, ntree=200)
+# 5.1 Build RF model using training(pureTrain) data
+# nodesize | ntree | auc
+#   25     |  200  | 0.9207
+#   50     |  200  | 0.9343
+#   75     |  200  | 0.9355
+#  100     |  200  | 0.9378
+#  150     |  200  | 0.9360
+#  100     |  250  | 0.9379
+modelRF = randomForest(Popular ~ ., data=pureTrain, nodesize=100, ntree=250)
 
-# 5.2 Make predictions on training data
-predictRF = predict(modelRF, data=blogTrain)
-confusionRF = table(blogTrain$Popular, predictRF > 0.5)
-trainAccuracyRF = (confusionRF[1,1]+ confusionRF[2,2])/sum(confusionRF)
-trainAccuracyRF
-# 5.3 Write predictions into CSV file "submissionRF.csv"
-predictRF2 = predict(modelRF, newdata=blogTest,type='class')
-submissionRF = data.frame(UniqueID=blogTest$UniqueID, Probability1=predictRF2)
+
+
+# 5.2 Make predictions on validation data
+predictRF = predict(modelRF, newdata=pureVal, type='class')
+predRF = prediction(predictRF, pureVal$Popular)
+aucRF = as.numeric(performance(predRF, "auc")@y.values)
+aucRF # 0.9379
+
+# 5.3 Build RF model using (pureTrain + pureVal) data
+modelRF = randomForest(Popular ~ ., data=blogTrain, nodesize=100, ntree=250)
+
+# 5.4 Write predictions into CSV file "submissionRF.csv"
+predictRF2 = predict(modelRF, newdata=blogTest, type='class')
+predictRF2[predictRF2 > 1] = 1.00 # fix floating-point arithmetic errors
+predictRF2[predictRF2 < 0] = 0.00 # fix floating-point arithmetic errors
+submissionRF = data.frame(UniqueID = testID, Probability1=predictRF2)
 write.csv(submissionRF, "submissionRF.csv", row.names=FALSE)
+
+### 6. Model 3: Support Vector Machine
+# 6.1 Build SVM model on pureTrain data
+modelSVM = svm(Popular ~ ., data=pureTrain)
+
+# 6.2 Make predictions on validation data
+predictSVM = predict(modelSVM, newdata= pureVal, type="class")
+predSVM = prediction(predictSVM, pureVal$Popular)
+aucSVM = as.numeric(performance(predSVM, "auc")@y.values)
+aucSVM
+
+# 6.3 Build SVM model using (pureTrain + pureVal) data
+modelSVM = svm(Popular ~ . - UniqueID, data=blogTrain)
+
+# 6.3 Write predictions into CSV file "submissionSVM.csv"
+predictSVM2 = predict(modelSVM, newdata=blogTest)
+predictSVM2[predictSVM2 > 1] = 1.00
+predictSVM2[predictSVM2 < 0] = 1e-8
+submissionSVM = data.frame(UniqueID = testID, Probability1=predictSVM2)
+write.csv(submissionSVM, "submissionSVM.csv", row.names=FALSE)
+
+
+### 7. Ensemble learning
+# 7.1 Define ensemble equation
+#   RF     |  LR   | auc
+#   0.6    |  0.4  | 0.9408
+#   0.7    |  0.3  | 0.9413
+#   0.75   |  0.25 | 0.9414
+#   0.8    |  0.2  | 0.9408
+
+predictEnsemble = 0.5 * predictRF + 0.5 * predictLR
+# 7.2 Make predictions on validation data
+predEnsemble = prediction(predictEnsemble, pureVal$Popular)
+aucEnsemble = as.numeric(performance(predEnsemble, "auc")@y.values)
+aucEnsemble # 0.9360
+# 7.3 Write predictions into CSV file "submissionEnsemble.csv"
+predictEnsemble2 = 0.5 * predictRF2 + 0.5 * predictLR2
+predictEnsemble2[predictEnsemble2 > 1] = 1.00
+predictEnsemble2[predictEnsemble2 < 0] = 0.00
+submissionEnsemble = data.frame(UniqueID = testID, Probability1 = predictEnsemble2)
+write.csv(submissionEnsemble, "submissionEnsemble.csv", row.names=FALSE)
 
